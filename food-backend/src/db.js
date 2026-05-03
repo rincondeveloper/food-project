a58b060f553bdc4b83bcb90936e50639ef700d62
rincondeveloper <4braham3mma@gmail.com>
2026-05-03 17:15:45 -0600

    fix ssl postgress render

--- food-backend/src/db.js
+++ food-backend/src/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false }
 });
 
 module.exports = pool;
