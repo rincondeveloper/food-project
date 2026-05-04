6e14f4178efdb18e4c9dec79c5ad0ad69a1ec1c2
rincondeveloper <4braham3mma@gmail.com>
2026-05-03 21:54:17 -0600

    useless files deleted

--- food-backend/src/index.js
+++ food-backend/src/index.js
@@ -8,9 +8,7 @@ const authRoutes = require('./routes/auth');
 const uploadsRoutes = require('./routes/uploads');
 
 const app = express();
-app.use(cors({
-  origin: ['https://food-project-plum.vercel.app', 'http://localhost:5173']
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/productos', productosRoutes);
